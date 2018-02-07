6f29a7d16b3d1b59ada9c54ebc7a682b13ba7d6b
liminjun <liminjun@cmss.chinamobile.com>
2018-02-07 22:13:15 +0800

    feat:creating first component.

---
+++ Getting-Started/APM-Start/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'pm-root',
+  template:`<div><h1>{{pageTitle}}</h1><div>My First Component</div></div>`
+})
+export class AppComponent{
+  pageTitle:string='Product Management';
+}
