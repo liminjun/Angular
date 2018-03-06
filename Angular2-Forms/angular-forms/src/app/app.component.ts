4db6052b9c9e34b25e0cb0530287fca0b73e217b
liminjun <v-leeli@hotmail.com>
2018-03-06 14:11:54 +0800

    feat:data binding in forms.

--- Angular2-Forms/angular-forms/src/app/app.component.ts
+++ Angular2-Forms/angular-forms/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {NgForm} from "@angular/forms";
+import { Employee } from './models/employee.model';
 
 
 
@@ -11,4 +12,13 @@ import {NgForm} from "@angular/forms";
 })
 export class AppComponent {
   title = 'app';
+  model=new Employee('Lee','Li',true,"1000",'english');
+
+  firstNameToUpperCase(value:string){
+    if(value.length>0){
+      this.model.firstName=value.charAt(0).toUpperCase()+value.slice(1);
+    }else{
+      this.model.firstName=value;
+    }
+  }
 }
