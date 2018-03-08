e065d2a5ad3447f9c2c922b3ed3356cf7ee426ed
liminjun <v-leeli@hotmail.com>
2018-03-08 21:42:10 +0800

    feat:adding post service.

--- Angular2-Forms/angular-forms/src/app/app.component.ts
+++ Angular2-Forms/angular-forms/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {NgForm} from "@angular/forms";
+import { NgForm } from "@angular/forms";
 import { Employee } from './models/employee.model';
 import { FormPoster } from './services/form-poster.service';
 
@@ -13,27 +13,35 @@ import { FormPoster } from './services/form-poster.service';
 })
 export class AppComponent {
   title = 'app';
-  model=new Employee('Lee','Li',true,"1000",'default');
-  hasPirmaryLanguageError=false;
+  model = new Employee('Lee', 'Li', true, "1000", 'default');
+  hasPirmaryLanguageError = false;
 
-  constructor(private formPoster:FormPoster){
+  constructor(private formPoster: FormPoster) {
 
   }
-  submitForm(form:NgForm){
-    console.log(this.model);
+  submitForm(form: NgForm) {
+    debugger;
+    this.validatePrimaryLanguage(this.model.primaryLanguage);
+    if (this.hasPirmaryLanguageError)
+      return;
+    this.formPoster.postEmployeeForm(this.model)
+    .subscribe(
+      data=>console.log('success',data),
+      error=>console.log('error',error)
+    );
   }
-  firstNameToUpperCase(value:string){
-    if(value.length>0){
-      this.model.firstName=value.charAt(0).toUpperCase()+value.slice(1);
-    }else{
-      this.model.firstName=value;
+  firstNameToUpperCase(value: string) {
+    if (value.length > 0) {
+      this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
+    } else {
+      this.model.firstName = value;
     }
   }
-  validatePrimaryLanguage(value){
-    if(this.model.primaryLanguage==='default'){
-      this.hasPirmaryLanguageError=true;
-    }else{
-      this.hasPirmaryLanguageError=false;
+  validatePrimaryLanguage(value) {
+    if (this.model.primaryLanguage === 'default') {
+      this.hasPirmaryLanguageError = true;
+    } else {
+      this.hasPirmaryLanguageError = false;
     }
   }
 }
--- Angular2-Forms/angular-forms/src/app/services/form-poster.service.ts
+++ Angular2-Forms/angular-forms/src/app/services/form-poster.service.ts
@@ -1,13 +1,28 @@
 import {Injectable} from "@angular/core";
-import {Http} from "@angular/http";
+import {Http, RequestOptions} from "@angular/http";
 import {Employee} from '../models/employee.model';
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class FormPoster{
+    handleError(error: any){
+       return Observable.throw(error.statusText);
+    }
+    extraData(res: Response) {
+        let body=res.json();
+        return body.fields||{};
+    }
     constructor(private http:Http){
 
     }
-    postEmployeeForm(employee:Employee){
+    postEmployeeForm(employee:Employee):Observable<any>{
         console.log('posting employee',employee);
+        let body=JSON.stringify(employee);
+        let headers=new Headers({'Content-Type':"application/json"});
+        let options=new RequestOptions({"Headers":headers});
+
+        return this.http.post('http://localhost:3100/postemployee',body,options)
+        .map(this.extraData)
+        .catch(this.handleError);
     }
 }
